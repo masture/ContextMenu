54c41cc57342582d7e2b9488587d07d8922e3380
Pankaj Kulkarni <kulkarnipanku@gmail.com>
2019-12-12 20:49:52 +0530

    Added a blue view and a working context menu. The actions just print the name of the action selected by the user.

--- Context Menu/ViewController.swift
+++ Context Menu/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addInteraction()
     }
 
+    
+    private func addInteraction() {
+        
+        let interaction = UIContextMenuInteraction(delegate: self)
+        menuView.addInteraction(interaction)
+        
+    }
+    
 
 }
 
+
+extension ViewController: UIContextMenuInteractionDelegate {
+    
+    func contextMenuInteraction(_ interaction: UIContextMenuInteraction, configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
+        
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil) { (segestdActions) -> UIMenu? in
+            
+            let share = UIAction(title: "Share", image: UIImage(systemName: "square.and.arrow.up")) { (action) in
+                print("Share menu option selected")
+            }
+            
+            let rename = UIAction(title: "Rename", image: UIImage(systemName: "square.and.pencil")) { (action) in
+                print("Rename menu option selected")
+            }
+            
+            let delete = UIAction(title: "Delete", image: UIImage(systemName: "trash"), attributes: .destructive) { (action) in
+                print("Delete menu option selected")
+            }
+            
+            return UIMenu(title: "", children: [share, rename, delete])
+            
+        }
+        
+    }
+    
+    
+}
